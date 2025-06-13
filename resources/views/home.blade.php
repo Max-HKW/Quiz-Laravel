da30ec9ac333bd661315cf5d94e04ae11a9aac51
Massimo Musso <Massimo.Musso@edu-its.it>
2025-06-13 15:00:28 +0200

    stile migliorato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,23 +5,23 @@
 
     <h2>Scegli una modalità:</h2>
 
-    <div style="display: flex; gap: 40px; margin-top: 20px;">
+    <div class="option-container">
 
         {{-- Modalità Allenamento --}}
-        <div>
+        <div class="option-content">
             <h3>Allenamento</h3>
-            <ul>
-                <li><a href="{{ route('quiz.training', ['type' => 'capital']) }}">Capitali</a></li>
-                <li><a href="{{ route('quiz.training', ['type' => 'flag']) }}">Bandiere</a></li>
+            <ul class="option-list">
+                <li class="option-item"><a href="{{ route('quiz.training', ['type' => 'capital']) }}">Capitali</a></li>
+                <li class="option-item"><a href="{{ route('quiz.training', ['type' => 'flag']) }}">Bandiere</a></li>
             </ul>
         </div>
 
         {{-- Modalità Gara --}}
-        <div>
+        <div class="option-content">
             <h3>Gara</h3>
-            <ul>
-                <li><a href="{{ route('quiz.game.selectDifficulty', ['type' => 'capital']) }}">Capitali</a></li>
-                <li><a href="{{ route('quiz.game.selectDifficulty', ['type' => 'flag']) }}">Bandiere</a></li>
+            <ul class="option-list">
+                <li class="option-item"><a href="{{ route('quiz.game.selectDifficulty', ['type' => 'capital']) }}">Capitali</a></li>
+                <li class="option-item"><a href="{{ route('quiz.game.selectDifficulty', ['type' => 'flag']) }}">Bandiere</a></li>
             </ul>
         </div>
 
